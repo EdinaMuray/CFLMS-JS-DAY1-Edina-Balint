b4766911417bcf621eb027e5fb19dd14623f313f
Balint Bolla <bolla.balint@gmail.com>
2020-05-25 10:08:57 +0200

    basic exercises

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log ("Hi my name is Balint");
+
+var age = 25;
+
+console.log ("and I am " + age + " years old");
+
+var playerNames = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+console.log ("The most valuable player of the match is " + playerNames [2]);
